c89cb8d9b3ac7d8ab5654144cc98af8867d05e33
Ameika <ameika2900@gmail.com>
2018-02-04 12:37:51 +0900

    whtjgus
    q

--- mail/app.js
+++ mail/app.js
@@ -2,9 +2,6 @@ const EWS = require('node-ews');
  
 // exchange server connection info
 const ewsConfig = {
-  username: 'ameika@lotte.net',
-  password: '@whtjgus584',
-  host: 'https://ews.lotte.net'
 };
  
 // initialize node-ews
@@ -31,9 +28,9 @@ const ewsArgs = {
       "Subject" : "와 미치겠다 진짜....",
       "Body" : {
         "attributes": {
-          "BodyType" : "Text"
+          "BodyType" : "HTML"
         },
-        "$value": "<h1>이게 말이 되냐...</h1><h5>하루종일 고민한게 5분만에 해결...</h5>답은 node-ews다!"
+        "$value": "<h1>이게 말이 되냐...</h1><h5>하루종일 고민한게 5분만에 해결...</h5>답은 node-ews다!<h3>아 알겠다 인증 안타면 아무데서나 되고 타면 ldcc망만 되는거 같은데?</h3>"
       },
       "ToRecipients" : {
         "Mailbox" : {
