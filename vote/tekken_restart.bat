07e3da46f1c51e898cdb9879bd106693bfe134f7
Ameika <ameika2900@gmail.com>
2018-04-12 21:09:37 +0900

    조서현
    철권 종료-재실행 완성

--- vote/tekken_restart.bat
+++ vote/tekken_restart.bat
@@ -1,17 +1,16 @@
 @echo off
 
-for /f  "tokens=2 delims=," %%i in ('tasklist /fo csv ^| find "zip"') do (
+echo 철권 종료중...
+for /f  "tokens=2 delims=," %%i in ('tasklist /fo csv ^| find "Tekken"') do (
 	echo %%i
-	taskkill /pid %%i
+	taskkill /pid %%i /f
 )
-for /f %%i in ('where /r d:\ msh.zip') do (
-	start %%i
-	exit
+
+echo 철권 경로찾는중...
+echo 스팀을 거치지 않은 실행이어서 스팀에서 경고창을 띄웁니다.
+echo 확인 누르시면 됩니다.
+for /f "eol=;" %%i in ('where /r e:\ tekken?7.exe') do (
+	SET tekken="%%i 7\TEKKEN 7.EXE"		
 )
-echo 'done'
-::
-::for /f  "tokens=2 delims=," %%i in ('tasklist /fo csv ^| find "cmd"') do (
-::	echo %%i
-::	taskkill /pid %%i
-::)
-::
+%tekken%
+exit
